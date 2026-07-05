70303caf3c95e143c1ba9bd2692f32aad51facdb
Lex Miin <hello@lexunix.me>
2026-07-05 12:12:31 +0300

    deps: update svelte, astro to latest versions

--- apps/gov/astro.config.mjs
+++ apps/gov/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import node from '@astrojs/node'
-import robots from 'astro-robots'
 import sitemap from '@astrojs/sitemap'
 
 import tailwindcss from '@tailwindcss/vite'
@@ -10,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://babynyar.gov.ua',
-  integrations: [react(), mdx(), sitemap(), robots()],
+  integrations: [react(), mdx(), sitemap()],
   adapter: node({ mode: 'standalone' }),
 
   vite: {
