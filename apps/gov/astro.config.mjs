496fd7fbd7259914805d9cea2febb4aad7293225
Lex Miin <hello@lexunix.me>
2026-09-01 02:23:49 +0300

    gov: optimize font delivery

--- apps/gov/astro.config.mjs
+++ apps/gov/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import node from '@astrojs/node'
@@ -9,6 +9,42 @@ import tailwindcss from '@tailwindcss/vite'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://babynyar.gov.ua',
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Roboto',
+      cssVariable: '--font-roboto',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/RobotoCondensed-Regular.woff2'],
+            weight: '400',
+            style: 'normal'
+          },
+          {
+            src: ['./src/assets/fonts/RobotoCondensed-Bold.woff2'],
+            weight: '700',
+            style: 'normal'
+          },
+          {
+            src: ['./src/assets/fonts/RobotoCondensed-Italic.woff2'],
+            weight: '400',
+            style: 'italic'
+          },
+          {
+            src: ['./src/assets/fonts/RobotoCondensed-LightItalic.woff2'],
+            weight: '300',
+            style: 'italic'
+          },
+          {
+            src: ['./src/assets/fonts/RobotoCondensed-BoldItalic.woff2'],
+            weight: '700',
+            style: 'italic'
+          }
+        ]
+      }
+    }
+  ],
   integrations: [
     react(),
     mdx(),
--- apps/gov/tailwind.config.cjs
+++ apps/gov/tailwind.config.cjs
@@ -2,13 +2,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
-  ],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Roboto"', ...defaultTheme.fontFamily.sans]
+        sans: ['var(--font-roboto)', ...defaultTheme.fontFamily.sans]
       }
     }
   },
